5c15153f1864e53f986d9a94868697c996c37cd1
Carsten Block <mail@carstenblock.org>
2010-12-30 23:45:21 +0100

    Correcting maven group-id setting for plugin

--- PowertacRandomGrailsPlugin.groovy
+++ PowertacRandomGrailsPlugin.groovy
@@ -1,4 +1,5 @@
 class PowertacRandomGrailsPlugin {
+  def groupId = 'org.powertac.plugins'
   // the plugin version
   def version = "0.1"
   // the version or versions of Grails the plugin is designed for
