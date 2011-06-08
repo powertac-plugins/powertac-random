092dbb4d8f0fa5b9e39c8ab69171a4deb53bb434
John Collins <jcollins@cs.umn.edu>
2011-06-08 14:34:33 -0500

    clean up config file

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,9 +31,3 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.13'
     }
 }
-
-grails.project.dependency.distribution = {
-  remoteRepository(id: "powertacPlugins", url: "http://ibwstinger.iw.uni-karlsruhe.de/artifactory/plugins-release-local/") {
-    authentication username: "[TODO: set your username]", password: "[TODO: set your password]"
-  }
-}
